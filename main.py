e218c252e90953bb8e0e011526c5045f428e4adc
Your Name <you@example.com>
2023-04-27 14:43:57 +0530

    cleaning of main.py

--- main.py
+++ main.py
@@ -13,43 +13,24 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/home',methods=["GET","POST"])
 def home():
-    data = {"name":"rakesh",
-            "backend":"flask"}
     if request.method == "GET":
         print("this is get")
         return jsonify(data)
     elif request.method == "POST":
-        # print("post is the method")
         py_code = request.get_json()
-        print(py_code['code'])
 
-        # print(py_code.split(":")[1].strip('"}'))
-        # py_code = py_code.split(":")[1].strip('"}')
-        # for data in py_code:
-        #     print(py_code[data])
-        # print(type(py_code))
-        # code = ''
-        # for char in py_code:
-        #     if char == '\\':
-        #         print("got him")
-        #     else:
-        #         code = code + char
-        # py_code = code
-        # print(py_code)
         with open("./online_code.py", "w") as f:
             f.write((py_code['code']))
+            
         output = open("./output.txt","w+")
         try:
             subprocess.call("python online_code.py",stdout=output,stderr=output)
         except Exception as e:
             print(e)
         output.close()
-        # # return request.data
-        # with open("./output.txt","r")as f:
-        #     output_data = f.read()
+        
         output = open("./output.txt","r")
         output_data = output.read()
-        # print(output_data)
         output.close()
         return output_data
 
